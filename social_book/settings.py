706bb05d6852795fc734a80a43aa442ec2ba1557
rebecaaras <rebecaaraujo2013@gmail.com>
2024-10-10 04:13:00 -0300

    fix: Error in 'core' app placement and typo correction

--- social_book/settings.py
+++ social_book/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core'
 ]
 
 ROOT_URLCONF = 'social_book.urls'
